49f182f6ae12f34f4ce7c7256f703b3da3470ee4
ManahilSiddiqui <manahil_siddiqui@hotmail.com>
2023-08-25 18:45:26 +0400

    Added conditional rendering for icons

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -11,12 +11,15 @@ export default function Accordion({ items }) {
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expandedIndex;
 
-       
+       const icon = <span>
+        {isExpanded ? 'DOWN' : 'LEFT'}
+       </span>
 
 
         return (
             <div key={item.id}>
                 <div onClick={() => handleClick(index)}>
+                    {icon}
                     {item.label}</div>
                 {isExpanded && <div>{item.content}</div>}
             </div>
