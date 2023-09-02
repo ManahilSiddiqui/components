849e3521c156ef2c972e1ea48620e57b2afb5d66
ManahilSiddiqui <manahil_siddiqui@hotmail.com>
2023-09-02 20:49:56 +0400

    Created a Link Component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import Link from './components/Link';
 
 
 export default function App() {
     return (
         <div>
-
+            <Link to="/accordion">Go to accordion</Link>
+            <Link to="/dropdown">Go to dropdown</Link>
         </div>
     )
 }
--- src/context/navigation.js
+++ src/context/navigation.js
@@ -1,11 +1,31 @@
-import { Children, createContext } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const NavigationContext = createContext();
 
 function NavigationProvider({children}) {
+    const [currentPath, setCurrentPath] = useState(window.location.pathname);
+    
+    useEffect(() => {
+        const handler = () => {
+            setCurrentPath(window.location.pathname);
+        };
+        window.addEventListener('popstate', handler);
+
+        return () => {
+            window.removeEventListener('popstate', handler);
+        };
+
+    }, []);
+
+    const navigate = (to) => {
+        window.history.pushState({}, '', to);
+        setCurrentPath(to);
+    };
+
+
     return ( 
     
-    <div> <NavigationContext.Provider value={{}}>
+    <div> <NavigationContext.Provider value={{ currentPath, navigate }}>
         {children}
     </NavigationContext.Provider>
     </div>
