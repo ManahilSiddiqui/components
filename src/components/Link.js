c99d28fc71295387ead97fb7716409e1ea2a8982
ManahilSiddiqui <manahil_siddiqui@hotmail.com>
2023-09-04 17:33:47 +0400

    Added styling to link component

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,10 +1,16 @@
+import classNames from "classnames";
 import { useContext } from "react";
 import NavigationContext from '../context/navigation';
 
 export default function Link({ to, children}) {
     const {navigate} = useContext(NavigationContext);
 
+    const classes = classNames('text-blue-500');
+
     const handleClick = (event) => {
+        if (event.metaKey || event.ctrlKey) {
+            return;
+        }
         event.preventDefault();
 
         navigate(to);
@@ -13,7 +19,9 @@ export default function Link({ to, children}) {
     return (
 
         <div>
-            <a onClick={handleClick}>{children}</a>
+            <a className={classes} href={to} onClick={handleClick}>
+                {children}
+                </a>
         </div>
     )
 }
