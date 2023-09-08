50c8e52e945cc3f59b031ca3244b54cc58899915
ManahilSiddiqui <manahil_siddiqui@hotmail.com>
2023-09-08 16:48:08 +0400

    Created a reusable Table

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,6 +6,7 @@ import Link from './Link';
         { label: 'Accordion', path: '/accordion' },
         { label: 'Buttons', path: '/buttons' },
         { label: 'Modal', path: '/modal' },
+        { label: 'Table', path: '/table'},
     ];
 
     const renderedLinks = links.map((link) => {
